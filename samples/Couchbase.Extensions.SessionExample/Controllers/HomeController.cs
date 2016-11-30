c7c6bd5e4ca2b58fc10abd6d2a2ed445f1175a5f
jeffrymorris <jeffrymorris@gmail.com>
2016-11-30 14:47:53 -0800

    Add example using distributed cache for Session state

---
+++ samples/Couchbase.Extensions.SessionExample/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace Couchbase.Extensions.SessionExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            HttpContext.Session.SetString("Test", "Session stored in couchbase!");
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = HttpContext.Session.GetString("Test");
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            HttpContext.Session.Remove("Test");
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ samples/Couchbase.Extensions.SessionExample/Startup.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Couchbase.Configuration.Client;
+using Couchbase.Extensions.Caching;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Couchbase.Extensions.SessionExample
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+
+            // Add the couchbase caching service
+            services.AddDistributedCouchbaseCache(opt =>
+            {
+                opt.BucketName = "default";
+                opt.Configuration = new ClientConfiguration
+                {
+                    Servers = new List<Uri>
+                    {
+                        new Uri("http://10.142.150.101:8091/")
+                    }
+                };
+                ClusterHelper.Initialize(opt.Configuration);
+                opt.Bucket = ClusterHelper.GetBucket(opt.BucketName);
+            });
+
+            services.AddSession();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            app.UseSession();
+
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
